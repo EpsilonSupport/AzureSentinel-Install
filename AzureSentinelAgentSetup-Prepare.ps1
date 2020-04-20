0811e913bd406743ed03f4d6c5f6148358bf527a
EpsilonSupport <63876888+EpsilonSupport@users.noreply.github.com>
2020-04-20 12:47:41 -0400

    Add files via upload

---
+++ AzureSentinelAgentSetup-Prepare.ps1
@@ -0,0 +1,19 @@
+﻿#create the directory --
+write-host "Creating Software Distribution directory ..."
+new-item -itemtype "directory" -path "C:\softwareDistribution\AzureSentinelAgent\Setup"
+
+#share the directory --
+write-host "Sharing Software Distribution directory ..."
+New-SmbShare -Name "softwareDistribution" -Path "C:\softwareDistribution\" -FullAccess "everyone"
+
+#Download batch file --
+write-host "Downloading Azure Sentinel Agent Batch File ..."
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/StaxMclean/SentinelTest/master/Install-AzureSentinel.bat" -outfile "C:\softwareDistribution\AzureSentinelAgent\Install-AzureSentinel.bat"
+
+#Download zip file --
+write-host "Downloading Azure Sentinel Agent Setup files ..."
+Invoke-WebRequest -Uri "https://github.com/StaxMclean/SentinelTest/releases/download/v1.0/AzureSentinelAgentSetup.zip" -outfile "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip"
+
+#EXTRACT AzureSentinelAgentSetup.zip -- 
+write-host "Extracting ZIP to C:\softwareDistribution\AzureSentinelAgent\Setup ..."
+Expand-Archive -LiteralPath "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip" -DestinationPath "C:\softwareDistribution\AzureSentinelAgent\Setup"
