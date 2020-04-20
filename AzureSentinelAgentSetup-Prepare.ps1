8d35beb2fa37e938b496b27e949cf663563aa8c2
EpsilonSupport <63876888+EpsilonSupport@users.noreply.github.com>
2020-04-20 16:52:27 -0400

    Update AzureSentinelAgentSetup-Prepare.ps1

--- AzureSentinelAgentSetup-Prepare.ps1
+++ AzureSentinelAgentSetup-Prepare.ps1
@@ -24,7 +24,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EpsilonSupport/Sentine
 
 #Download zip file --
 write-host "Downloading Azure Sentinel Agent Setup files ..."
-Invoke-WebRequest -Uri "https://github.com/EpsilonSupport/SentinelTest/releases/download/v1.0/AzureSentinelAgentSetup.zip" -outfile "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip"
+Invoke-WebRequest -Uri "https://github.com/EpsilonSupport/SentinelTest/releases/download/v1.1/AzureSentinelAgentSetup.zip" -outfile "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip"
 
 #EXTRACT AzureSentinelAgentSetup.zip -- 
 write-host "Extracting ZIP to C:\softwareDistribution\AzureSentinelAgent\Setup ..."
