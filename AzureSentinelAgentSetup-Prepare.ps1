8d1d5b7ce6b0bd7fc2660cdb86ce714acc5c1874
EpsilonSupport <63876888+EpsilonSupport@users.noreply.github.com>
2020-04-20 17:13:46 -0400

    Update AzureSentinelAgentSetup-Prepare.ps1

--- AzureSentinelAgentSetup-Prepare.ps1
+++ AzureSentinelAgentSetup-Prepare.ps1
@@ -8,32 +8,82 @@ $key=Read-Host -Prompt 'Azure Sentinel Workspace ID Key'
 
 
 #Enable TLS1.2 --
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+write-host "Enabling TLS 1.2 ..."
+try {
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #create the directory --
 write-host "Creating Software Distribution directory ..."
-new-item -itemtype "directory" -path "C:\softwareDistribution\AzureSentinelAgent\Setup"
+try {
+  new-item -itemtype "directory" -path "C:\softwareDistribution\AzureSentinelAgent\Setup"
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #share the directory --
 write-host "Sharing Software Distribution directory ..."
-New-SmbShare -Name "softwareDistribution" -Path "C:\softwareDistribution\" -FullAccess "everyone"
+try {
+  New-SmbShare -Name "softwareDistribution" -Path "C:\softwareDistribution\" -FullAccess "everyone"
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #Download batch file --
 write-host "Downloading Azure Sentinel Agent Batch File ..."
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EpsilonSupport/SentinelTest/master/Install-AzureSentinel.bat" -outfile "C:\softwareDistribution\AzureSentinelAgent\Install-AzureSentinel.bat"
+try {
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EpsilonSupport/SentinelTest/master/Install-AzureSentinel.bat" -outfile "C:\softwareDistribution\AzureSentinelAgent\Install-AzureSentinel.bat"
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #Download zip file --
 write-host "Downloading Azure Sentinel Agent Setup files ..."
-Invoke-WebRequest -Uri "https://github.com/EpsilonSupport/SentinelTest/releases/download/v1.1/AzureSentinelAgentSetup.zip" -outfile "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip"
+try {
+  Invoke-WebRequest -Uri "https://github.com/EpsilonSupport/SentinelTest/releases/download/v1.1/AzureSentinelAgentSetup.zip" -outfile "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip"
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #EXTRACT AzureSentinelAgentSetup.zip -- 
 write-host "Extracting ZIP to C:\softwareDistribution\AzureSentinelAgent\Setup ..."
-Expand-Archive -LiteralPath "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip" -DestinationPath "C:\softwareDistribution\AzureSentinelAgent\Setup"
+try {
+  Expand-Archive -LiteralPath "C:\softwareDistribution\AzureSentinelAgent\AzureSentinelAgentSetup.zip" -DestinationPath "C:\softwareDistribution\AzureSentinelAgent\Setup"
+}
+catch {
+  WriteWarning $Error[0]
+}
 
 #Mounting Drive --
 write-host "Mounting Drive ..."
-net use X: \\$serverName\softwareDistribution\AzureSentinelAgent
+try {
+  net use X: \\$serverName\softwareDistribution\AzureSentinelAgent
+}
+catch {
+  WriteWarning $Error[0]
+}
+
+#Installing Agent --
 write-host "Installing Agent ..."
-X:\Install-AzureSentinel.bat $id $key
+try {
+  X:\Install-AzureSentinel.bat $id $key
+}
+catch {
+  WriteWarning $Error[0]
+}
+
+#Unmounting Drive
 write-host "Unmounting Drive ..."
-net use x: /d
+try {
+  net use x: /d
+}
+catch {
+  WriteWarning $Error[0]
+}
